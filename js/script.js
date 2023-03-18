d3ba1287bedfd68fa27a8ad6c784a83bf1af5eee
TalanetsLiza <yarmantovich1999@mail.ru>
2023-03-18 16:12:05 +0300

    add: complete task 2

--- js/script.js
+++ js/script.js
@@ -8,3 +8,14 @@
     console.log(newStr); 
 
 }
+
+{
+    // TASK 2
+    console.log("\n");
+
+    const date = "2025-12-31";
+    const regex = /(\d\d\d\d)-(\d\d)-(\d\d)/; // /d - это любая цифра
+    const newDate = date.replace(regex, "$3/$2/$1");
+	console.log(newDate); 
+
+}
